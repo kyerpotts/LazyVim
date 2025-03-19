179ee93b625035af527df8cc2dfb56d3422e80f2
Kyer Potts <kyer.potts@dpc.wa.gov.au>
2025-03-19 15:54:07 +0800

    Remove cyberdream from config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -20,35 +20,6 @@ return {
       },
     },
   },
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("monokai-pro").setup({
-        transparent_background = true,
-        terminal_colors = false,
-        devicons = true,
-        styles = {
-          comment = { italic = true },
-          keyword = { italic = true },
-          type = { italic = true },
-          storageclass = { italic = true },
-          structure = { italic = true },
-          parameter = { italic = true },
-          annotation = { italic = true },
-          tag_attribute = { italic = true },
-        },
-        filter = "pro",
-        background_clear = {
-          "toggleterm",
-          "notify",
-          "nvim-tree",
-          "notify",
-        },
-      })
-    end,
-  },
   -- {
   --   "scottmckendry/cyberdream.nvim",
   --   lazy = false,
